cd5c680b8536eb94e7b6e06b71451277db100695
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-15 10:59:18 -0400

    Added routes to application

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -31,7 +31,30 @@ function App() {
       <div className={
         `dark-bg-main-bg bg-main-bg min-h-screen w-full ${isMenuActive ? 'md:ml-72' : 'flex-2'}`
       }>
-
+        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+          NavBar
+        </div>
+      </div>
+      <div>
+        <Routes>
+          <Route path='/' element="ECommerce" />
+          <Route path='/ecommerce' element="ECommerce" />
+          <Route path='/orders' element="Orders" />
+          <Route path='/employees' element="Employees" />
+          <Route path='/customers' element="Customers" />
+          <Route path='/kanban' element="KanBan" />
+          <Route path='/editor' element="Editor" />
+          <Route path='/calendar' element="Calendar" />
+          <Route path='/color-picker' element="ColorPicker" />
+          <Route path='/line' element="Line" />
+          <Route path='/area' element="Area" />
+          <Route path='/bar' element="Bar" />
+          <Route path='/pie' element="Pie" />
+          <Route path='/financial' element="Financial" />
+          <Route path='/color-mapping' element="ColorMapping" />
+          <Route path='/pyramid' element="Pyramid" />
+          <Route path='/stacked' element="Stacked" />
+        </Routes>
       </div>
     </div >
   );
