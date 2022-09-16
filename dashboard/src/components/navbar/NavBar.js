c67fee224dde34f6c97cb6f4111d3c8b6bef112a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-16 10:53:03 -0400

    Imported packages and icons into the NavBar component

--- dashboard/src/components/navbar/NavBar.js
+++ dashboard/src/components/navbar/NavBar.js
@@ -1,3 +1,14 @@
+import { useEffect } from 'react'
+import { AiOutlineMenu } from 'react-icons/ai'
+import { FiShoppingCart } from 'react-icons/fi'
+import { BsChatLeft } from 'react-icons/bs'
+import { RiNotification3Line } from 'react-icons/ri'
+import { MdKeyboardArrowDown } from 'react-icons/md'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+
+import avatar from '../../data/avatar.jpeg'
+import { Cart, Chat, Notification, UserProfile } from '../index'
+import { useStateContext } from '../../contexts/ContextProvider'
 
 const NavBar = () => {
   return (
