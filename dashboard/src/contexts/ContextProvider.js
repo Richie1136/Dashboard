929180cc7edc1644e69fb17abd3dd2426570b17d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-16 10:37:14 -0400

    To make it easier to pass data down

---
+++ dashboard/src/contexts/ContextProvider.js
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+
+const StateContext = createContext()
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false
+}
+
+export const ContextProvider = ({ children }) => {
+
+  const [activeMenu, setActiveMenu] = useState(true)
+
+
+  const context = {
+    activeMenu,
+  }
+
+  return (
+    <StateContext.Provider value={context}>
+      {children}
+    </StateContext.Provider>
+  )
+
+}
+
+export const useStateContext = () => useContext(StateContext)
