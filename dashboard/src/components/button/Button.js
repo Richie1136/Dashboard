0b569d2e68dc9174a17af6f7faf0b81263208de1
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-19 11:18:31 -0400

    Stying button

--- dashboard/src/components/button/Button.js
+++ dashboard/src/components/button/Button.js
@@ -1,7 +1,10 @@
 
-const Button = ({ color, bgColor, text, borderRadius }) => {
+const Button = ({ color, bgColor, text, borderRadius, size }) => {
   return (
-    <div>Button</div>
+    <button className={`text-${size} p-3 hover: drop-shadow-xl`} type="button"
+      style={{ backgroundColor: bgColor, color: color, text: text, borderRadius: borderRadius }}>
+      {text}
+    </button>
   )
 }
 
--- dashboard/src/pages/ecommerce/Ecommerce.js
+++ dashboard/src/pages/ecommerce/Ecommerce.js
@@ -20,7 +20,7 @@ const Ecommerce = () => {
             </div>
           </div>
           <div className="mt-6">
-            <Button color='white' bgColor='blue' text='Download' borderRadius="10px" />
+            <Button color='white' bgColor='blue' text='Download' borderRadius="10px" size="md" />
           </div>
         </div>
       </div>
