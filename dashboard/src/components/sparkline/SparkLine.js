4a21c67b4896decef7760a1e51a25b4fcd8d125b
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-20 10:34:03 -0400

    Importing packages from syncfusion

--- dashboard/src/components/sparkline/SparkLine.js
+++ dashboard/src/components/sparkline/SparkLine.js
@@ -1,3 +1,5 @@
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
+
 
 const SparkLine = ({ currentColor, type, height, width, data, color }) => {
   return (
