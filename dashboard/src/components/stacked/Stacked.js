af12e027d3107c36caa7bac4e6b8cd203c24f2c4
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-20 10:59:17 -0400

    Importing packages from syncfusion

--- dashboard/src/components/stacked/Stacked.js
+++ dashboard/src/components/stacked/Stacked.js
@@ -1,5 +1,7 @@
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from "@syncfusion/ej2-react-charts"
 
-const Stacked = () => {
+
+const Stacked = ({ width, height }) => {
   return (
     <div>Stacked</div>
   )
