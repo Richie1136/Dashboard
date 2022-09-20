d274faccd998129d7576f3a073e9921ff78b57fe
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-20 10:49:21 -0400

    Building out the Sparkline component

--- dashboard/src/components/sparkline/SparkLine.js
+++ dashboard/src/components/sparkline/SparkLine.js
@@ -1,9 +1,11 @@
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
 
 
-const SparkLine = ({ currentColor, type, height, width, data, color }) => {
+const SparkLine = ({ currentColor, type, height, width, data, color, id }) => {
   return (
-    <div>SparkLine</div>
+    <SparklineComponent tooltipSettings={{ visible: true, format: '${x}: data ${y}', trackLineSettings: { visible: true } }} type={type} xName='x' yName='y' dataSource={data} border={{ color: currentColor, width: 2 }} id={id} height={height} width={width} lineWidth={1} valueType="Numeric" fill={color}>
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
   )
 }
 
