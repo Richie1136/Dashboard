8e3460cebddc39fc00738bf3f87fa055affae5df
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-21 11:57:05 -0400

    Importing data from dummy file and importing the header component

--- dashboard/src/pages/customers/Customers.js
+++ dashboard/src/pages/customers/Customers.js
@@ -1,5 +1,9 @@
 import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from "@syncfusion/ej2-react-grids"
 
+import { customersData, customersGrid } from "../../data/dummy"
+
+import { Header } from "../../components"
+
 const Customers = () => {
   return (
     <div>Customers</div>
