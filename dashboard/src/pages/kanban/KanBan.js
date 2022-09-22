df82361f7f8703a35b509f55d6f9415410de07ce
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-22 10:57:54 -0400

    Created a empty kanban board

--- dashboard/src/pages/kanban/KanBan.js
+++ dashboard/src/pages/kanban/KanBan.js
@@ -7,7 +7,16 @@ import { Header } from '../../components'
 
 const KanBan = () => {
   return (
-    <div>KanBan</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="App" title="Kanban" />
+      <KanbanComponent dataSource={kanbanData}>
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
   )
 }
 
