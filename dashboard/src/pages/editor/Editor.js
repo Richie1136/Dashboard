5919bd3c10e1922138fa0987f477977bfadee768
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-22 11:23:23 -0400

    Importing psckages from syncfusion richtexteditor, imported Header component and imported data from the dummy file

--- dashboard/src/pages/editor/Editor.js
+++ dashboard/src/pages/editor/Editor.js
@@ -1,3 +1,9 @@
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor'
+
+import { EditorData } from '../../data/dummy'
+
+import { Header } from '../../components'
+
 
 const Editor = () => {
   return (
