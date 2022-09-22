e8eb06d59bd69038ff72d091a1a5e1f74bdebe6f
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-22 10:24:51 -0400

    Importing packages from syncfusion inside of calendar

--- dashboard/src/pages/calendar/Calendar.js
+++ dashboard/src/pages/calendar/Calendar.js
@@ -1,3 +1,4 @@
+import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWee, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-calendars'
 
 const Calendar = () => {
   return (
--- dashboard/src/pages/customers/Customers.js
+++ dashboard/src/pages/customers/Customers.js
@@ -6,7 +6,17 @@ import { Header } from "../../components"
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Customers" />
+      <GridComponent dataSource={customersData} allowPaging allowSorting toolbar={['Delete']} editSettings={{ allowDeleting: true, allowEditing: true }} width="auto">
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
+      </GridComponent>
+    </div>
   )
 }
 
