0d0aa7db6188c884c2f8e3af30a7424f85a9ea88
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-22 10:51:40 -0400

    Importing data from the dummy file, import header component and importing packages from syncfusion

--- dashboard/src/pages/kanban/KanBan.js
+++ dashboard/src/pages/kanban/KanBan.js
@@ -1,3 +1,9 @@
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban'
+
+import { kanbanData, kanbanGrid } from '../../data/dummy'
+
+import { Header } from '../../components'
+
 
 const KanBan = () => {
   return (
