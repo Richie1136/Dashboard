cd467610086f3513864d2d06f68ebc2270fa66ca
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-22 11:17:24 -0400

    Added a keyfield prop to the KanBanComponent to display all of the different tasks

--- dashboard/src/pages/kanban/KanBan.js
+++ dashboard/src/pages/kanban/KanBan.js
@@ -9,7 +9,7 @@ const KanBan = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category="App" title="Kanban" />
-      <KanbanComponent dataSource={kanbanData}>
+      <KanbanComponent keyField='Status' id="kanban" dataSource={kanbanData} cardSettings={{ contentField: 'Summary', headerField: 'Id' }}>
         <ColumnsDirective>
           {kanbanGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
