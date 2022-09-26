9e4dc9b1a652f28ed0a7dccfad2089873e069472
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-26 10:48:01 -0400

    Render out the different themeColors

--- dashboard/src/components/ThemeSettings.js
+++ dashboard/src/components/ThemeSettings.js
@@ -33,8 +33,8 @@ const ThemeSettings = () => {
             {themeColors?.map((item, index) => (
               <TooltipComponent key={index} content={item.name} position="TopCenter">
                 <div className="relative mt-3 cursor-pointer flex gap-5 items-center">
-                  <button>
-                    <BsCheck className={`ml-2 text-2xl text-white ${item.color === currentColor ? 'block' : 'hidden'}`} />
+                  <button type='button' className="h-10 w-10 rounded-full cursor-pointer" style={{ backgroundColor: item.color }}>
+                    <BsCheck className={`ml-2 text-2xl text-white ${true ? 'block' : 'hidden'}`} />
                   </button>
                 </div>
               </TooltipComponent>
