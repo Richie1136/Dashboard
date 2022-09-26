92599aa532adf9b31620177b043a4de811db3366
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-26 10:44:09 -0400

    Mapping over the themeColors and Wrapping div and button with a TooltipComponent

--- dashboard/src/components/ThemeSettings.js
+++ dashboard/src/components/ThemeSettings.js
@@ -22,6 +22,24 @@ const ThemeSettings = () => {
             <input type="radio" id='light' name="theme" value="Light" className="cursor-pointer" onChange={() => { }} checked={true} />
             <label htmlFor="light" className="ml-2 text-md cursor-pointer">Light</label>
           </div>
+          <div className="mt-4">
+            <input type="radio" id='dark' name="theme" value="Dark" className="cursor-pointer" onChange={() => { }} checked={true} />
+            <label htmlFor="Dark" className="ml-2 text-md cursor-pointer">Dark</label>
+          </div>
+        </div>
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+          <p className="font-semibold text-lg">Theme Colors</p>
+          <div className="flex gap-3">
+            {themeColors?.map((item, index) => (
+              <TooltipComponent key={index} content={item.name} position="TopCenter">
+                <div className="relative mt-3 cursor-pointer flex gap-5 items-center">
+                  <button>
+                    <BsCheck className={`ml-2 text-2xl text-white ${item.color === currentColor ? 'block' : 'hidden'}`} />
+                  </button>
+                </div>
+              </TooltipComponent>
+            ))}
+          </div>
         </div>
       </div>
     </div>
