b2cfe46f421ba9ef6aacf7085019d18ba0379343
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-27 11:37:27 -0400

    Rendering the data for the line chart and customized the x and y axis

--- dashboard/src/components/linecharts/LineCharts.js
+++ dashboard/src/components/linecharts/LineCharts.js
@@ -1,10 +1,19 @@
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip } from "@syncfusion/ej2-react-charts"
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, LineSeries, Inject, DateTime, Legend, Tooltip } from "@syncfusion/ej2-react-charts"
+
+import { lineCustomSeries, LinePrimaryYAxis, LinePrimaryXAxis } from "../../data/dummy"
 
 
 
 const LineCharts = () => {
   return (
-    <div>LineCharts</div>
+    <ChartComponent id='line-chart' height="420px" primaryXAxis={LinePrimaryXAxis} primaryYAxis={LinePrimaryYAxis}>
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
