6fa22c763b29a70ddde612c24ca9db00c100cea3
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-27 11:21:14 -0400

    Created a LineCharts component and imported packages from syncfusion react charts

--- dashboard/src/components/linecharts/LineCharts.js
+++ dashboard/src/components/linecharts/LineCharts.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip } from "@syncfusion/ej2-react-charts"
+
+
 
 const LineCharts = () => {
   return (
