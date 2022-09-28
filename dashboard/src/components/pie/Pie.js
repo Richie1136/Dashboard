3d016d15aeb2e6f39144c7bb3d4c4caaea94153a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-28 10:56:26 -0400

    Importing packages from react-charts and importing data from my context provider

--- dashboard/src/components/pie/Pie.js
+++ dashboard/src/components/pie/Pie.js
@@ -1,3 +1,5 @@
+import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, AccumulationDataLabel, AccumulationLegend, PieSeries, Inject, AccumulationTooltip } from "@syncfusion/ej2-react-charts"
+
 
 const Pie = () => {
   return (
--- dashboard/src/pages/piechart/PieChart.js
+++ dashboard/src/pages/piechart/PieChart.js
@@ -1,7 +1,26 @@
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, PieSeries, Inject, DateTime, Legend, ColumnSeries, Tooltip, Category, DataLabel } from "@syncfusion/ej2-react-charts"
+
+import { pieChartData } from "../../data/dummy"
+
+import { useStateContext } from "../../contexts/ContextProvider"
+
+import { Header } from "../../components"
+import Pie from "../../components"
+
+
 
 const PieChart = () => {
+
+  const { currentMode } = useStateContext()
+
   return (
-    <div>PieChart</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category="Bar" title="Olympic Medal Counts - RIO" />
+      <div className="w-full">
+        <Pie id='chart-pie' data={pieChartData} legendVisiblity height='full' />
+
+      </div>
+    </div>
   )
 }
 
