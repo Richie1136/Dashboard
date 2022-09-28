58ce3dbca7c0a4d75e006d64e926c414f907a7f5
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-28 11:52:51 -0400

    Imported packages from react-charts and imported data from the dummy file as well as imported Header component and import useStateContext. Completed the Pyramid component

--- dashboard/src/pages/stacked/Stacked.js
+++ dashboard/src/pages/stacked/Stacked.js
@@ -1,7 +1,24 @@
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, BarSeries, Inject, DateTime, Legend, ColumnSeries, Tooltip, Category, DataLabel, RangeColorSettingsDirective, RangeColorSettingDirective } from "@syncfusion/ej2-react-charts"
+
+import { stackedChartData, stackedPrimaryXAxis, stackedPrimaryYAxis, stacked, stackedCustomSeries } from "../../data/dummy"
+
+import { useStateContext } from "../../contexts/ContextProvider"
+
+import { Header } from "../../components"
+import StackedChart from "../../components/stacked/Stacked"
+
+
 
 const Stacked = () => {
+
+
   return (
-    <div>Stacked</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category="Stacked" title="Revenue Breakdown" />
+      <div className="w-full">
+        <StackedChart />
+      </div>
+    </div>
   )
 }
 
