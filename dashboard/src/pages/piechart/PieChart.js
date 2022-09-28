d241ca8f1f3a8fa8f663e0f9cf713f13e10a87ac
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-28 11:18:40 -0400

    Importing packages from react-charts and importing data from my context provider into my Pie component and then using the Pie component inside of the PieChart component

--- dashboard/src/pages/piechart/PieChart.js
+++ dashboard/src/pages/piechart/PieChart.js
@@ -1,21 +1,14 @@
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, PieSeries, Inject, DateTime, Legend, ColumnSeries, Tooltip, Category, DataLabel } from "@syncfusion/ej2-react-charts"
 
 import { pieChartData } from "../../data/dummy"
-
-import { useStateContext } from "../../contexts/ContextProvider"
-
-import { Header } from "../../components"
-import Pie from "../../components"
+import { Header, Pie } from "../../components"
 
 
 
 const PieChart = () => {
 
-  const { currentMode } = useStateContext()
-
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-      <Header category="Bar" title="Olympic Medal Counts - RIO" />
+      <Header category="Pie" title="Project Cost Breakdown" />
       <div className="w-full">
         <Pie id='chart-pie' data={pieChartData} legendVisiblity height='full' />
 
