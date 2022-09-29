9605dc657df0bb2f1afac3387aa55103bddd7d1d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-29 11:24:25 -0400

    Importing icons from react icons, calling the useStateContext and importing the button component and grabbing chatData from the dummy file

--- dashboard/src/components/chat/Chat.js
+++ dashboard/src/components/chat/Chat.js
@@ -1,3 +1,8 @@
+import { Button } from '../index'
+import { MdOutlineCancel } from 'react-icons/md'
+import { chatData } from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
+
 
 const Chat = () => {
   return (
