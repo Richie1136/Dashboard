39f49cf110f5f5595bd1400882a7b1e339cec57b
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-29 11:52:46 -0400

    Added styling to the Notification component

--- dashboard/src/components/notification/Notification.js
+++ dashboard/src/components/notification/Notification.js
@@ -5,8 +5,17 @@ import { useStateContext } from "../../contexts/ContextProvider"
 
 
 const Notification = () => {
+
+  const { currentColor } = useStateContext()
+
   return (
-    <div>Notification</div>
+    <div className="nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-3">
+          <p className="font-semibold text-lg dark:text-gray-200">Notifications</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
