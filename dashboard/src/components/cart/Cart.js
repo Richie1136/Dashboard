e04996563449d90228f593744612086c04ba0f91
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-29 10:24:06 -0400

    Importing icons from react icons, calling the useStateContext and importing the button component and grabbing cartData from the dummy file

--- dashboard/src/components/cart/Cart.js
+++ dashboard/src/components/cart/Cart.js
@@ -1,3 +1,9 @@
+import { MdOutlineCancel } from "react-icons/md"
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai"
+import { useStateContext } from "../../contexts/ContextProvider"
+import { cartData } from "../../data/dummy"
+import { Button } from '../button/Button'
+
 
 const Cart = () => {
   return (
