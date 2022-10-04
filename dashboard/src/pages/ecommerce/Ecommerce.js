84510dec9580160a941e10d8a836f4a472e00823
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-04 10:25:23 -0400

    Mapping over all of the weekly stats and rendering out the icons along with the title and description

--- dashboard/src/pages/ecommerce/Ecommerce.js
+++ dashboard/src/pages/ecommerce/Ecommerce.js
@@ -3,9 +3,10 @@ import { GoPrimitiveDot } from "react-icons/go"
 import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns'
 
 
-import { earningData, SparklineAreaData, ecomPieChartData, dropdownData, recentTransactions } from "../../data/dummy"
+import { earningData, SparklineAreaData, ecomPieChartData, dropdownData, recentTransactions, weeklyStats } from "../../data/dummy"
 import { Button, SparkLine, Stacked, Pie, LineChart, LineCharts } from "../../components"
 import { useStateContext } from "../../contexts/ContextProvider"
+import { IoIosMore } from "react-icons/io"
 
 
 
@@ -155,6 +156,29 @@ const Ecommerce = () => {
           </div>
           <div className="md:w-full overflow-auto">
             <LineCharts />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        <div className="md:w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">Weekly Sales</p>
+            <button type='button' className="text-xl font-semibold text-gray-500">
+              <IoIosMore />
+            </button>
+          </div>
+          <div className="mt-10">
+            {weeklyStats?.map((item) => (
+              <div key={item.ittle} className="flex justify-between mt-4 w-full">
+                <div className="flex gap-4">
+                  <button type='button' style={{ background: item.iconBg }} className='text-2xl hover:drop-shadow-xl text-white rounded-full p-3'>{item.icon}</button>
+                  <div>
+                    <p className="text-md font-semibold">{item.title}</p>
+                    <p className="text-sm text-gray-400">{item.desc}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
